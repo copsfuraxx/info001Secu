9525d6d47a8326c12be823099e0b355b9e173f54
Jules Finck <jules.finck@hotmail.fr>
2023-11-15 09:05:38 +0100

    jules 4

--- client.py
+++ client.py
@@ -1,32 +1,32 @@
 import socket
 import ssl
+import fnmatch
 
 def verify_certificate(cert_string, hostname):
     # Obtain the peer certificate
     cert = ssock.getpeercert()
 
-    # Extract the subject and verify it matches the expected hostname
-    subject = dict(x[0] for x in cert['subject'])
-    common_name = subject.get('commonName', '')
+    san_entries = [entry[1] for entry in cert.get('subjectAltName', []) if entry[0] == 'DNS']
     
-    if common_name == hostname:
+    if any(fnmatch.fnmatchcase(hostname, entry) for entry in san_entries):
         print("Certificate verification successful. Domain matches.")
         return True
     else:
-        print(f"Certificate verification failed. Expected: {hostname}, Actual: {common_name}")
+        print(f"Certificate verification failed.")
         return False
 
 if __name__ == '__main__':
     hostname = input('Adresse du serveur : ')
     # PROTOCOL_TLS_CLIENT requires valid cert chain and hostname
-    context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-    context.load_verify_locations('certificates/root-ca-lorne.pem')
+    # context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+    # context.load_verify_locations('certificates/root-ca-lorne.pem')
+    context = ssl.create_default_context()
 
     with socket.create_connection((hostname, 8888)) as sock:
         with context.wrap_socket(sock, server_hostname=hostname) as ssock:
             ssock.do_handshake()
-            if verify_certificate(ssock, hostname):
-                msg = input("Message : ")
-                ssock.sendall(msg.encode(encoding='UTF-8',errors='strict'))
+            # if verify_certificate(ssock, hostname):
+            msg = input("Message : ")
+            ssock.sendall(msg.encode(encoding='UTF-8',errors='strict'))
             ssock.close()
     
